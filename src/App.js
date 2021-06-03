df662ed7a86b05f3ced1b152f33b21dc3405d88b
Connor Holland <supersoccergoalie123@gmail.com>
2021-06-03 11:31:25 -0600

    commit tree 3a88ed3101507a24a1eac5c231db5d7ff0ada8c9

--- src/App.js
+++ src/App.js
@@ -1,1 +1,18 @@
 // create your App component here
+import React from 'react'
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>Hello</div>
+        )
+    }
+
+    componentDidMount() {
+        fetch('http://api.open-notify.org/astros.json (Links to an external site.)')
+        .then(resp => resp.json)
+        .then(data => data)
+    }
+}
+
+export default App
